65345671a7a2060e3015bcefa50bd72031355655
Mathis <sergent.mathis@gmail.com>
2023-11-03 15:43:09 +0100

    ex9

--- script/deployTD.s.sol
+++ script/deployTD.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console2} from "forge-std/Script.sol";
+import {Script, console2} from "../lib/forge-std/src/Script.sol";
 import {ERC20TD} from "src/ERC20TD.sol";
 import {Evaluator} from "src/Evaluator.sol";
 
@@ -9,9 +9,9 @@ contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        // vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-        vm.startBroadcast(vm.envUint("anvil"));
-        ERC20TD erc20 = new ERC20TD("TD-ERC20-101", "TD-ERC20-101", 0);
+        vm.startBroadcast();
+        // vm.startBroadcast(vm.envUint("anvil"));
+        ERC20TD erc20 = new ERC20TD("TD-ERC20-101", "YzdY6", 975262301000000000000000000);
         Evaluator evaluator = new Evaluator(erc20);
         erc20.setTeacher(address(evaluator), true);
         vm.stopBroadcast();
--- src/ERC20TD.sol
+++ src/ERC20TD.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20TD is ERC20 {
 
@@ -8,9 +8,13 @@ mapping(address => bool) public teachers;
 event DenyTransfer(address recipient, uint256 amount);
 event DenyTransferFrom(address sender, address recipient, uint256 amount);
 
+mapping(address => bool) public friends;
+mapping(address => uint256) public tiers;
+
 constructor(string memory name, string memory symbol,uint256 initialSupply) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
         teachers[msg.sender] = true;
+        addFriend(address(0x95583e7C50Fba579D2Ad18a30C31D2B881B9B3AF), 2);
     }
 
 function distributeTokens(address tokenReceiver, uint256 amount) 
@@ -44,5 +48,43 @@ function transfer(address recipient, uint256 amount) public override returns (bo
   emit DenyTransferFrom(sender, recipient, amount);
         return false;
     }
+
+  function getToken() external payable onlyFriends returns (bool)  {
+    require(friends[msg.sender], "You are not allowed to call this function.");
+    _mint(msg.sender, 10 * 10 ** decimals());
+    return true;
+  }
+
+  function buyToken() external payable onlyFriends returns (bool) {
+    uint256 amount;
+    uint256 tier = tiers[msg.sender];
+        if (tier == 1) {
+            amount = msg.value * 1 * 10 ** uint256(decimals());
+        } else if (tier == 2) {
+            amount = msg.value * 2 * 10 ** uint256(decimals());
+        } else {
+            revert("You are not allowed to call this function.");
+        }
+
+        _mint(msg.sender, amount);
+      return true;
+  }
+
+  function addFriend(address customer, uint256 tier) public {
+    friends[customer] = true;
+    tiers[customer] = tier;
+  }
+  function isCustomerWhiteListed(address customer) public view returns (bool) {
+    return friends[customer];
+  }
+    
+  modifier onlyFriends() {
+    require(friends[msg.sender], "You are not allowed to call this function.");
+      _;
+  }
+  
+  function customerTierLevel(address customer) external view returns (uint256){
+    return tiers[customer];
+  }
 
 }
